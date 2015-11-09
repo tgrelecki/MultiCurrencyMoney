ee5958c8aaa8debea5d0296a5172fa6c2c8808f8
tgrelecki <tgrelecki@gmail.com>
2015-11-08 19:35:56 -0600

    Initial Working Build

---
+++ MultiCurrencyMoney.Test/MultiplicationTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiCurrencyMoney.Test
+{
+    [TestClass]
+    public class MultiplicationTests
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            var five = new Dollar(5);
+            five.times(2);
+
+            Assert.AreEqual(10, five.Amount);
+        }
+
+
+    }
+}
---
+++ MultiCurrencyMoney/Dollar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MultiCurrencyMoney
+{
+    public class Dollar
+    {
+        public int Amount;
+
+        public Dollar(int value)
+        {
+            Amount = value;
+        }
+
+        public void times(int value)
+        {
+            Amount *= value;
+        }
+    }
+}
